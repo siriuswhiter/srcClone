af0dd52e07fc4536f0fe6de72c4ab4a0b022e708
huaketiger <73773196+huaketiger@users.noreply.github.com>
2022-05-16 10:16:51 +0800

    crann4.0

--- crANN.py
+++ crANN.py
@@ -2,8 +2,15 @@ import lshash
 
 
 class CRANN(object):
-    def __init__(self, vectors):
-        self.vectors = vectors
+    def __init__(self, vectors1, vectors2=None):
+        if vectors2 is None:
+            self.v2 = False
+        else:
+            self.v2 = True
+        if self.v2:
+            self.vectors = vectors1 + vectors2
+        else:
+            self.vectors = vectors1
         self.MAX = len(self.vectors[0])
         self.lsh = lshash.LSHash(6, len(self.vectors[0]))
         self.init()
@@ -31,8 +38,37 @@ class CRANN(object):
                     return 0
         return distance
 
+    def query2(self, query_point):
+        match_num = 0
+        result = self.lsh.query(query_point, distance_func="hamming")
+        if len(result) >= 1:
+            for i in result:
+                count = 0
+                for j in range(len(query_point)):
+                    if query_point[j] == i[0][j]:
+                        count += 1
+                distance = len(query_point) - count
+                if distance <= 1:
+                    match_num += 1
+        return match_num
+
     def query_bool(self, query_point):
         if self.query(query_point) > 1:
             return False
         else:
             return True
+
+    def count_probability(self, vectors2=None):
+        if vectors2 is None:
+            count = 0
+            for v in self.vectors:
+                match_num = self.query2(v)
+                if match_num > 1:
+                    count += 1
+            return float(count) / float(len(self.vectors))
+        else:
+            match_num = 0
+            for vector in vectors2:
+                if self.query_bool(vector):
+                    match_num += 1
+            return float(match_num) / float(len(vectors2))
--- srcClone.py
+++ srcClone.py
@@ -4,6 +4,7 @@ from parse import Parser
 from slice import Vector
 from crANN import CRANN
 
+
 # input: filename
 # output: AllSP
 def src2sps(file):
@@ -16,9 +17,8 @@ def src2sps(file):
             # print(result)
             parser = Parser()
             re = parser.parse(result.split("\n"))
-    
-    return re
 
+    return re
 
 
 # input: AllSP
@@ -26,12 +26,13 @@ def src2sps(file):
 def sps2vecs(all_sp):
     vecset = []
 
-    for file_name, file_sp in all_sp.file_sp_dict.items ():
-        for func_name, func_sp in file_sp.func_sp_dict.items ():
+    for file_name, file_sp in all_sp.file_sp_dict.items():
+        for func_name, func_sp in file_sp.func_sp_dict.items():
             for var_name in func_sp.var_sp_dict:
-                print("{0}-{1}-{2} : {3}".format(file_name, func_name, var_name, func_sp.get_vvector_by_name(var_name).vec()))
-            print("{0}-{1} : {2}".format(file_name, func_name,func_sp.get_vector().vec()))
-            
+                print("{0}-{1}-{2} : {3}".format(file_name, func_name, var_name,
+                                                 func_sp.get_vvector_by_name(var_name).vec()))
+            print("{0}-{1} : {2}".format(file_name, func_name, func_sp.get_vector().vec()))
+
     return vecset
 
 
@@ -43,19 +44,15 @@ def main(file1, file2):
     slicing_vectors_2 = sps2vecs(slice_profiles_2)
 
     # similarity and matching
-    crANN = CRANN(slicing_vectors_1)
-    match_num = 0
-    for vector in slicing_vectors_2:
-        if crANN.query_bool(vector):
-            match_num += 1
-    probability = float(match_num)/float(len(slicing_vectors_2))
+    # crANN = CRANN(slicing_vectors_1)    crANN.count_probability(slicing_vectors_2)
+    # crANN = CRANN(slicing_vectors_1,slicing_vectors_2)    crANN.count_probability()
+    crANN = CRANN(slicing_vectors_1, slicing_vectors_2)
+    probability = crANN.count_probability()
     print(probability)
     return probability
 
 
-
-
 if __name__ == "__main__":
-    if len(sys.argv)<3:
+    if len(sys.argv) < 3:
         exit("Usage: python3 srcClone.py file1 file2")
     main(sys.argv[1], sys.argv[2])
